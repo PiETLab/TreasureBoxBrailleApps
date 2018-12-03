353e9b96221e47a73f137314ccfd6cdecf1c4f12
Sunny Lee <sunnylee0123@gmail.com>
2018-12-03 14:34:16 -0500

    Fix the letter "c" in braille alphabet.

--- src/enamel/BrailleCell.java
+++ src/enamel/BrailleCell.java
@@ -53,7 +53,7 @@ public class BrailleCell {
 	private void initializeAlphabet() {
 		alphabet.put('a', "10000000");
 		alphabet.put('b', "11000000");
-		alphabet.put('c', "10100000");
+		alphabet.put('c', "10010000");
 		alphabet.put('d', "10011000");
 		alphabet.put('e', "10001000");
 		alphabet.put('f', "11010000");
